426209fccb2c1fb6c55829b6c2dbd9d7bc7dd8db
cblunt <chris@chrisblunt.com>
2009-06-18 12:57:26 +0100

    Fixed db migrations to match model

--- db/migrate/20090618114317_create_users.rb
+++ db/migrate/20090618114317_create_users.rb
@@ -3,8 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :email_address
       t.integer :status
-      t.boolean :is_admin
+      t.boolean :admin
 
       t.timestamps
     end
