6ca9f3b65574b0ebcb42b665c397d9c14ab39f50
cblunt <chris@chrisblunt.com>
2009-06-19 08:32:57 +0100

    Added additional search criteria to users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     params[:search] ||= {} 
+    params[:search][:admin] = (params[:search][:admin] == "true" ? true : nil)
 
     @users = User.search(:all, :filters => params[:search])
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def label_for_status(status)
+    case status
+      when 1 then return "Viewer"
+      when 2 then return "Member"
+      when 3 then return "Subscriber"
+      when 4 then return "Publisher"
+      when 5 then return "Editor"
+    end
+  end
 end
